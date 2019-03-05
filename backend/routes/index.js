95bfecfe001e650f0631666302f9bfb4aa5d1887
User <user@Users-MacBook-Pro-2.local>
2019-03-05 13:53:07 -0500

    connected db

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const config = require('../config');
+const connection = mysql.createConnection(config);
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
