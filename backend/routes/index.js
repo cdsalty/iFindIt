41dff150dcb032819dcaa72895f14cb4162b6c80
cdsalty <soltiscd@gmail.com>
2019-03-05 15:20:03 -0500

    login route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,39 @@ const mysql = require('mysql');
 const config = require('../config');
 const connection = mysql.createConnection(config);
 connection.connect();
+const bcrypt = require('bcrypt');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Send data to database via POST
+router.post('/login', (req, res, next)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  const passwordQuery = `SELECT * FROM users WHERE email = ?`
+  connection.query(passwordQuery, [email], (err, results)=>{
+    if (err){
+     throw error;
+    }else {
+      if (results.length === 0) {
+        // Two options to give user: Redirect to registration or inform user they are not in our system.
+        res.json({
+          message: "invalidUser",
+        });    
+      }else{
+        const passwordMatch = bcrypt.compareSync(password, results[0].password)
+        if (!passwordMatch){
+          res.json({
+            message: "wrongPassword",
+          })
+        }else{
+          //possible sessions
+          res.json({
+            message: "welcome"
+          })
+        }
+      }
+      
+    };
+  })
 });
+
 
 module.exports = router;
