b4c43adc9e0a6acfb0267777ace852f420feb944
Zac Crosby <Zacros7164@gmail.com>
2019-03-08 10:27:34 -0500

    hooked redux up to app

---
+++ front-end2/src/reducers/authReducer.js
@@ -0,0 +1,24 @@
+// This is a reducer.
+// To make a reducer:
+// 1. make a function that has 2 params:
+//     - state (usually set a default)
+//     - action
+// 2. The state param comtians THIS reducers state. Just like a variable.
+// 3. The action param will contain whatever the object the dispatched action sent us
+//     - type (REQUIRED)
+//     - payload (data/JSON/stuff)
+// 4. Check the action.type to see if I care.
+// 4a. If I care, return some new state
+// 4b. If I don't care, return state (old state)
+// when dispatcher runs this function, you HAVE to return something even if no change
+
+function authReducer(state = null, action){
+    if(action.type === "LOGIN"){
+        // do stuff
+        return state
+    }else{
+        return state
+    }
+}
+
+export default authReducer;
---
+++ front-end2/src/reducers/rootReducer.js
@@ -0,0 +1,16 @@
+// ====== THIS IS THE ROOT REDUCER ======
+
+// To make a RootReducer:
+// 1. Get combineReducers method from redux
+// 2. Call combineReducers method an pass it an object
+// 3. Each key/property of the object, will be a single piece of application/redux state
+// 4. Each value will be a single reducer (imported)
+
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
+
+export default rootReducer;
