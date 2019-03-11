9860aaa59f8704274e29c15956e8c7618510e8d0
cdsalty <soltiscd@gmail.com>
2019-03-11 14:03:51 -0400

    webScraper

---
+++ backend/GoogleScrape copy/index.js
@@ -0,0 +1,32 @@
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+
+// const url = 'https://www.google.com/search?q=tangled&oq=tangled&aqs=chrome..69i57j69i61j69i59j69i60j69i61.1366j0j1&sourceid=chrome&ie=UTF-8';
+// // const url ='https://news.ycombinator.com/';
+
+
+const axios = require(`axios`);
+const cheerio = require(`cheerio`);
+
+
+const url = `https://www.google.com/search?q=tangled`;
+// const url =‘https://news.ycombinator.com/’;
+
+const config = {
+    headers: {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1',
+    }
+};
+
+axios.get(url,config)
+   .then(response=>{
+    const $ = cheerio.load(response.data);
+    console.log($('.phXTff').text())
+    // console.log(response.data);
+   })
+   .catch(error=>{
+       console.log(error);
+   })
+
+   // Now we are getting back two different divs; we need to map through the divs to return a list of each.
