317ff9d598f6dae77fa59dee093251c179d447cc
cdsalty <soltiscd@gmail.com>
2019-03-13 15:56:15 -0400

    navbar

--- front-end2/src/components/Navbar.js
+++ front-end2/src/components/Navbar.js
@@ -8,26 +8,35 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div className="container-fluid banner">
-                    <div className="row d-flex space-between">
-                        <div className="logo-holder col-sm-10 col-md-8">
-                            <img src="/images/logo.png" alt="logo" />
-                        </div>
-                        <div className="button-holder col-sm-2 col-md-3">
-                            <Link to="/login"><button className="button">Login</button></Link>
-                            <Link to="/register"><button className="button">Register</button></Link>
-                        </div>
-                    </div>
+            <nav className="navbar-nav">
+                <div className="myContainer">
                     <div className="row">
-                        <div className="col-12 d-flex justify-content-center">
+                        <div className="Search">
                             <div className="search-bar">
                                 <form onSubmit={this.movieSearchHandler}>
                                     <input type="text" id="movieTitle" name="movieTitle" placeholder="Enter your movie search here" />
                                 </form>
                             </div>
                         </div>
+                        </div>
+                        <div className="row">
+                        <div className="logo-holder">
+                            <img src="/images/logo.png" alt="logo" />
+                        </div>
+                        </div>
+                        <div className="row">
+                        <div className="loginHolder">
+                            <div className="login-holder">
+                                <Link to="/login"><button className="button">Login</button></Link>
+                            </div>
+                            <div className="registerHolder">
+                                <Link to="/register"><button className="button">Register</button></Link>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+            </nav>
 
         )
     };
