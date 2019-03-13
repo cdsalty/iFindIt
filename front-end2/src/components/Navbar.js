5ffd36ce6b832ce9a9c2f6744d44665d6a0c6e0d
Zac Crosby <Zacros7164@gmail.com>
2019-03-13 13:23:08 -0400

    style changes

--- front-end2/src/components/Navbar.js
+++ front-end2/src/components/Navbar.js
@@ -8,20 +8,27 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div className="navbar">
-                <div className="logo-holder">
-                    <img src="/images/logo.png" alt="logo" />
+            <div className="container-fluid banner">
+                    <div className="row d-flex space-between">
+                        <div className="logo-holder col-sm-10 col-md-8">
+                            <img src="/images/logo.png" alt="logo" />
+                        </div>
+                        <div className="button-holder col-sm-2 col-md-3">
+                            <Link to="/login"><button className="button">Login</button></Link>
+                            <Link to="/register"><button className="button">Register</button></Link>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-center">
+                            <div className="search-bar">
+                                <form onSubmit={this.movieSearchHandler}>
+                                    <input type="text" id="movieTitle" name="movieTitle" placeholder="Enter your movie search here" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="button-holder">
-                    <Link to="/login"><button className="button">Login</button></Link>
-                    <Link to="/register"><button className="button">Register</button></Link>
-                </div>
-                <div className="search-bar">
-                    <form onSubmit={this.movieSearchHandler}>
-                        <input type="text" id="movieTitle" name="movieTitle" placeholder="Enter your movie search here" />
-                    </form>
-                </div>
-            </div>
+
         )
     };
 }
