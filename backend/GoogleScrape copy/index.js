6a5529fc0d63d2c9fa69da2fdf4c464862b3108d
Zac Crosby <Zacros7164@gmail.com>
2019-03-13 09:10:38 -0400

    scraper

--- backend/GoogleScrape copy/index.js
+++ backend/GoogleScrape copy/index.js
@@ -15,7 +15,6 @@ function scraper(movie) {
             'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1',
         }
     };
-<<<<<<< HEAD
     return new Promise((resolve, reject) => {
         axios.get(url, config)
             .then(response => {
@@ -43,23 +42,6 @@ function scraper(movie) {
                 console.log(error);
                 reject(error)
             })
-=======
-    return new Promise((resolve, reject)=>{
-        axios.get(url,config)
-        .then(response=>{
-            const $ = cheerio.load(response.data);
-            // console.log($)
-            const results = $('.phXTff').text();
-            // console.log(results)
-            // console.log("results " + results)
-            resolve(results);
-            // console.log(response.data);
-        })
-        .catch(error=>{
-            console.log(error);
-            reject(error)
-        })
->>>>>>> a53a8eb5e9ba7c0437532f2713f96004c522d7c0
         // console.log(results)
     })
 }
